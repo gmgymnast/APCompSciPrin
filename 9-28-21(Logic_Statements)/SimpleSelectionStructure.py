0564ca2b300448d732ecc297b6c9178897e74293
Gianluca Mancini <gfmancini@att.net>
2021-09-28 13:30:45 -0400

    SimpleSelectionStructure

---
+++ 9-28-21(Logic_Statements)/SimpleSelectionStructure.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+legalDriveAge = 16
+
+if age < legalDriveAge:
+    print("You Cant Drive! You Need To Be", legalDriveAge)
+else:
+    print("You Are Old Enough to Drive")
+    
