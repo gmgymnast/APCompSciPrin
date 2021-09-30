67adc402716262f6f01ba3eca8bf368f54e9b072
gmgymnast <30958522+gmgymnast@users.noreply.github.com>
2021-09-30 13:08:47 -0400

    test

--- 9-29-21/Lesson/lesson.py
+++ 9-29-21/Lesson/lesson.py
@@ -9,20 +9,20 @@ if str1 <= str2 and str1 <= str3:
         third = str3
     else:
         second = str3
-#         third = str2
-# elif str2 <= str1 and str2 <= str3:
-#     first = str2
-#     if str1 <= str3:
-#         second = str1
-#         third = str3
-#     else:
-#         second = str3
-#         third = str1
-# else:
-#     first = str3
-#     if str2 <= str1:
-#         second = str2
-#         third = str2
+         third = str2
+ elif str2 <= str1 and str2 <= str3:
+     first = str2
+     if str1 <= str3:
+         second = str1
+         third = str3
+     else:
+         second = str3
+         third = str1
+ else:
+     first = str3
+     if str2 <= str1:
+         second = str2
+         third = str2
 
 print(first)
 print(second)
