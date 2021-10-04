380339e9318017fc4b0cb3fdc8007bfebe99306f
Gianluca Mancini <gfmancini@att.net>
2021-10-04 13:07:19 -0400

    random

---
+++ 10-4-21/Lesson/random.py
@@ -0,0 +1,12 @@
+import random
+
+# for i in range(10):
+#     value = random.random()
+#     print(value)
+
+for i in range(10):
+    d1 = random.randint(1, 6)
+    d2 = random.randint(1, 6)
+
+    print(d1, d2)
+
