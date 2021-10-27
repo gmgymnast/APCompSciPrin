6b82ab3d9b3ea5c6aaa7c2beb61b218bd9c6a055
gmgymnast <gfmancini@att.net>
2021-10-26 23:59:37 -0400

    h

--- 10-26-21 (Random Numbers)/Assignment/rock_paper_scissors.py
+++ 10-26-21 (Random Numbers)/Assignment/rock_paper_scissors.py
@@ -1,10 +1,10 @@
 from random import *
-
-playerC = input("Enter Your Choice: ").lower()
-compC = choice(["rock", "paper", "scissors"])
-
+playerC, compC = input("Enter Your Choice: ").lower(), choice(["rock", "paper", "scissors"])
+ 
 if playerC == "rock": playerC = "a"
 if compC == "rock": compC = "a"
 
 if playerC == compC: print("Tie") 
+elif playerC == "a" and compC == "scissors": print("Player Won")
+elif playerC == "scissors" and CompC == "a": print("Computer Won")
 else: print("Player Won") if(compC < playerC) else print("Computer Won") 
