6b69e9e87229180e0a23b62b53923e0a1d360afd
Gianluca <gfmancini@att.net>
2021-11-04 13:19:33 -0400

    11-4-21

--- 11-1-21/Assignment/every_combo_of_letters_in_word.py
+++ 11-1-21/Assignment/every_combo_of_letters_in_word.py
@@ -1,5 +1,2 @@
-word = 'rum'
-
-for length in range(1, len(word)+1):
-    for pos in range(0, len(word)-length+1):
-        print(word[pos:pos+length])
+w = input("Enter a Word: ")
+[print(w[pos:pos+length]) for length in range(1, len(w)+  1) for pos in range(0, len(w) - length + 1)]
--- 11-1-21/Assignment/syllable_counter.py
+++ 11-1-21/Assignment/syllable_counter.py
@@ -1,3 +1,3 @@
-w, sCounter = input("Enter a Word: "), 1 
-for n, i in enumerate(w): sCounter += 0 if w[n - 1] in "aeiouAEIOU" else + 1 if w[n] in "aeiouAEIOU" and n != len(w) - 1 else 0 
-print("Number of Syllables:", sCounter)
+w, sCount, noW = input("Enter a Word: "), 1, "aeiouyAEIOUY"
+for n, i in enumerate(w): sCount += 0 if w[n - 1] in noW else + 1 if i in noW and (i in "aiouyAIOUY" and n == len(w) - 1) else 0 
+print("Number of Syllables:", sCount)
